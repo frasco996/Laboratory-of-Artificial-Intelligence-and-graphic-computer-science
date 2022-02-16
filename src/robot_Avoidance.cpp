61f6dd76f7098e2273b7a5888df31fc9549b3786
frasco996 <fabiofraschetti99@gmail.com>
2022-02-16 22:41:19 +0100

    aggiunta commenti

--- src/robot_Avoidance.cpp
+++ src/robot_Avoidance.cpp
@@ -16,18 +16,18 @@ geometry_msgs::Twist receivedV;
 const int N=200;
 
 void laser_scan_sub_callback(const sensor_msgs::LaserScan::ConstPtr& msg){
-
 	if (!res) return;
 	res = false;
-
+	//definisco variabili
 	tf::TransformListener list;
 	laser_geometry::LaserProjection proj;
 	sensor_msgs::PointCloud cloud;
+	tf::StampedTransform ob;
+	//i punti degli ostacoli rispetto al laser
 	proj.transformLaserScanToPointCloud("base_laser_link",*msg,cloud,list);
 
-	tf::StampedTransform ob;
-
 	try{
+		//calcolo la trasformata per ottenere la posizione degli ostacoli
 		list.waitForTransform("base_footprint", "base_laser_link", ros::Time(0), ros::Duration(1.0));
 		list.lookupTransform("base_footprint", "base_laser_link", ros::Time(0), ob); 
 
@@ -36,57 +36,61 @@ void laser_scan_sub_callback(const sensor_msgs::LaserScan::ConstPtr& msg){
 		ROS_ERROR("%s", ex.what());
 		return;
 	    }
-
+	//richiamo la funzione convertPose2D
 	Eigen::Isometry2f laserTrans = convertPose2D(ob);
+	// posizione dagli oggetti del laser
 	Eigen::Vector2f p_min; 
-
+	//cloud points 540 
 	p_min(0) = cloud.points[540].x; 
 	p_min(1) = cloud.points[540].y;
+	//posizione dell'oggetto
 	auto ob_pos = laserTrans * p_min;
 	float ob_dist = sqrt(std::pow(ob_pos(0),2)+std::pow(ob_pos(1),2));
-
+	//range fra 340 e 740
 	const float min = cloud.points.size()/2-N;
 	const float max = cloud.points.size()/2+N;
 
 	for(int i=min;i<=max;i++){
 	  Eigen::Vector2f v;
-
+	  
 	  v(0) = cloud.points[i].x; 
 	  v(1) = cloud.points[i].y;  
 
 	  auto ob_posA = laserTrans * v;
 	  float ob_distA = sqrt(std::pow(ob_posA(0),2)+std::pow(ob_posA(1),2));
-
+	  //ricerca del minimo
 	  if(ob_distA < ob_dist){
 	    ob_dist = ob_distA;
 	    ob_pos = ob_posA;
 	  }
 	}
-	if(ob_dist < 0.5 && receivedV.linear.x > 0 ){
+	if(ob_dist < 0.45 && receivedV.linear.x > 0 ){
+	// intensità della forza
 	  float force_int = (1.0 / ob_dist) * 0.3 ;
 	  float forceX = -(ob_pos(0) / ob_dist) * force_int;
 	  float forceY = -(ob_pos(1) / ob_dist) * force_int;
 
 	  geometry_msgs::Twist msg_send;
-
+	//definisco messaggi della velocità negativa
 	  msg_send.linear.x = receivedV.linear.x + forceX;
 	  msg_send.linear.y = receivedV.linear.y + forceY;
 	  msg_send.linear.z = receivedV.linear.z;
-
+	//definisco la velocità angolare
 	  if(ob_pos(1) > 0)
-	    msg_send.angular.z = - force_int; 
+	    msg_send.angular.z = - 0.5*force_int; 
 	  else if (ob_pos(1) < 0)
-	    msg_send.angular.z = force_int; 
-	  ROS_INFO("msg_send.linear.x %f",msg_send.linear.x);
-
+	    msg_send.angular.z = 0.5*force_int;
+	//pubblico e il robot esegue
 	  speed.publish(msg_send);
 	}
 	else{
+		//non succede niente e pubblico semplicemente la velocità
   		speed.publish(receivedV);
 	}
 }
 
 void vel_cmd_sub_callback(const geometry_msgs::Twist::ConstPtr& msg){ 
+	// ricevo la velocità e la salvo 
 	res = true;
 	receivedV = *msg;
 }
